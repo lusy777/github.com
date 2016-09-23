58a4145e069bf1a29733e793b5530e48ca0f1b5e
Lyudmila <lyudmila93@list.ru>
2016-09-23 12:21:13 +0300

    лаб 1

---
+++ src/main/ru.bstu.iitus.pv41.ludmila/lab1/Lab1.java
@@ -0,0 +1,11 @@
+package lab1;
+
+/**
+ * Created by 123 on 23.09.2016.
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
